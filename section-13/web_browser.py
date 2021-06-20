55120c906b9dd929a4be84fcde00529836656ba7
Nathan Pham <nathanpham.me@gmail.com>
2021-06-20 13:22:31 -0700

    finished mapit script

--- section-13/web_browser.py
+++ section-13/web_browser.py
@@ -1,3 +1,9 @@
 import webbrowser
+import pyperclip
+import sys
 
-webbrowser.open("https://google.com")
+arguments = sys.argv
+maps_api = "https://www.google.com/maps/search/"
+address = arguments.pop() if len(arguments) > 1 else pyperclip.paste()
+
+webbrowser.open(maps_api + "+".join(address.split()))
