7fde4269455e2fbf8721b34ffd205d67480097f6
Nathan Pham <nathanpham.me@gmail.com>
2021-06-20 14:25:56 -0700

    fetch data

---
+++ section-13/parsing_html.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+shop = "https://www.spaceposters.co/"
+
+def get_space_product(product_number=1):
+    response = requests.get(shop)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        title_element = soup.select(f"#posters > div > div > div > div:nth-child({product_number}) > div.prodcut-list-content-wrapper > h2")[0]
+        price_element = soup.select(f"#posters > div > div > div > div:nth-child({product_number}) > div.prodcut-list-content-wrapper > div.product-list-price")[0]
+
+        return title_element.text.strip(), float("".join([char for char in price_element.text.strip() if char.isdigit() or char == "."]))
+
+    return "unknown product", 0.0
+
+print(get_space_product(1))    
+print(get_space_product(2))    
