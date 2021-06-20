3def7335307dc306cba099613a00b8a059503ddc
Nathan Pham <nathanpham.me@gmail.com>
2021-06-20 13:48:43 -0700

    request romeo & juliet

---
+++ section-13/requests_module.py
@@ -0,0 +1,14 @@
+import requests
+
+book = "https://www.gutenberg.org/files/1112/1112.txt"
+
+response = requests.get(book)
+handle = open("files/romeo-book.txt", "wb")
+
+if response.status_code == 200:
+    for chunk in response.iter_content(100_000): # 100_000 bytes
+        handle.write(chunk)
+else:
+    print("error", response.status_code)
+
+handle.close()
