2f0c1865fa44cb8654a43cfea874d9357ce9bbbd
Nathan Pham <nathanpham.me@gmail.com>
2021-06-21 15:31:05 -0700

    create_sheet parameters

--- section-14/edit_excel.py
+++ section-14/edit_excel.py
@@ -2,9 +2,12 @@ import openpyxl as xl
 
 workbook = xl.Workbook()
 sheet_name = workbook.sheetnames[0]
-sheet = workbook[sheet_name]
+sheet_1 = workbook[sheet_name]
 
-sheet['A1'].value = "Hello"
-sheet['A2'].value = "World"
+sheet_1['A1'].value = "Hello"
+sheet_1['A2'].value = "World"
+
+sheet_2 = workbook.create_sheet(index=0, title="New Sheet Name")
+# sheet_2.title = "New Sheet Name"
 
 workbook.save("files/section-14/edit_excel.xlsx")
